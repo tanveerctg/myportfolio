8675e9528f2b46ff529ebd420dbc4c31d676e766
tanveerul-hoque <tanveerctg2014@gmail.com>
2019-03-19 11:12:36 +0600

    Updates

---
+++ precache-manifest.28af40fcb0833abf9746e6c878c8f201.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6923d550a41d50c026d7e765afd78af5",
+    "url": "/tanveerctg/myportfolio/static/media/hero.6923d550.jpg"
+  },
+  {
+    "revision": "5211369068e944857625ba86ac024306",
+    "url": "/tanveerctg/myportfolio/static/media/github2.52113690.png"
+  },
+  {
+    "revision": "2d01f9a5b7f46c61fd0d12de9bfa9ec2",
+    "url": "/tanveerctg/myportfolio/static/media/burgersterritory.2d01f9a5.png"
+  },
+  {
+    "revision": "3d030847ff5238c4fd9b8abd498c51fd",
+    "url": "/tanveerctg/myportfolio/static/media/budgetapp2.3d030847.jpg"
+  },
+  {
+    "revision": "5bf23194333f8648968e26f15a67a395",
+    "url": "/tanveerctg/myportfolio/static/media/1.5bf23194.jpeg"
+  },
+  {
+    "revision": "b5a66912d56bca968598",
+    "url": "/tanveerctg/myportfolio/static/js/runtime~main.b5a66912.js"
+  },
+  {
+    "revision": "2421383c55055946e828",
+    "url": "/tanveerctg/myportfolio/static/js/main.2421383c.chunk.js"
+  },
+  {
+    "revision": "1e0910060b65089f84eb",
+    "url": "/tanveerctg/myportfolio/static/js/1.1e091006.chunk.js"
+  },
+  {
+    "revision": "2421383c55055946e828",
+    "url": "/tanveerctg/myportfolio/static/css/main.044780ff.chunk.css"
+  },
+  {
+    "revision": "bab6eb965869dafa387f519173fc0c76",
+    "url": "/tanveerctg/myportfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9048c4b3b5c23bb6e1dc8f7cf30dfe8d.js"
+  "/tanveerctg/myportfolio/precache-manifest.28af40fcb0833abf9746e6c878c8f201.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/tanveerctg/myportfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
