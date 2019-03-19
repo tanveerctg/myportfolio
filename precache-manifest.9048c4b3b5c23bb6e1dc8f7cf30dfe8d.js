85c9dab930df92fa5466ade29963275d3041f05b
tanveerul-hoque <tanveerctg2014@gmail.com>
2019-03-19 10:58:44 +0600

    Updates

---
+++ precache-manifest.9048c4b3b5c23bb6e1dc8f7cf30dfe8d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6923d550a41d50c026d7e765afd78af5",
+    "url": "/static/media/hero.6923d550.jpg"
+  },
+  {
+    "revision": "5211369068e944857625ba86ac024306",
+    "url": "/static/media/github2.52113690.png"
+  },
+  {
+    "revision": "2d01f9a5b7f46c61fd0d12de9bfa9ec2",
+    "url": "/static/media/burgersterritory.2d01f9a5.png"
+  },
+  {
+    "revision": "3d030847ff5238c4fd9b8abd498c51fd",
+    "url": "/static/media/budgetapp2.3d030847.jpg"
+  },
+  {
+    "revision": "5bf23194333f8648968e26f15a67a395",
+    "url": "/static/media/1.5bf23194.jpeg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fee0bcd270c3520f582c",
+    "url": "/static/js/main.fee0bcd2.chunk.js"
+  },
+  {
+    "revision": "c40cb6b7071ac6624779",
+    "url": "/static/js/1.c40cb6b7.chunk.js"
+  },
+  {
+    "revision": "fee0bcd270c3520f582c",
+    "url": "/static/css/main.044780ff.chunk.css"
+  },
+  {
+    "revision": "3abfbc2252dd8c9ccc3c644f62f5ea52",
+    "url": "/index.html"
+  }
+];
